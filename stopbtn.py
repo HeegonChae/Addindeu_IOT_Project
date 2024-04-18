287808e579ac1503f5fedbe895e33df275571d4b
hyejji <hyejijjang10@gmail.com>
2024-04-18 10:13:24 +0900

    REFACTOR : send recv button

--- stopbtn.py
+++ stopbtn.py
@@ -10,7 +10,6 @@ from PyQt5.QtWidgets import QWidget
 # 시리얼 포트 설정
 serial_port = '/dev/ttyACM0'
 baud_rate = 9600
-bt_serial = serial.Serial(serial_port, baud_rate, timeout=1)
 
 from_class = uic.loadUiType("stopbtn.ui")[0]
 
@@ -19,16 +18,30 @@ class WindowClass(QMainWindow, from_class) :
         super().__init__()
         self.setupUi(self)
 
+        self.bt_serial = serial.Serial(serial_port, baud_rate, timeout=1)
+
         self.btnEStop.clicked.connect(self.btnPressed)
     
     def btnPressed(self):
         #self.editState.clear()
         self.textBrowser.append("Button Pressed " + self.currentTime())
         try:
-                bt_serial.write(b'Button Pressed\n')
-                self.textBrowser.append("Data sent to Arduino" + self.currentTime())
+             self.sendData()
+             self.recvData()
         except Exception as e:  
                 self.textBrowser.append("Error:", e)
+    
+    def sendData(self):
+         self.bt_serial.write('Button Pressed\n'.encode())
+         self.textBrowser.append("Data sent to Arduino" + self.currentTime())
+         time.sleep(0.1)
+    
+    def recvData(self):
+          if(self.bt_serial.readable()):
+            recv = self.bt_serial.readline().decode().strip('\r\n')
+            if(len(recv) > 0):
+                self.textBrowser.append("recv : " + str(recv))
+         
     
     def currentTime(self):
         return QDateTime.currentDateTime().toString("yy-MM-dd hh:mm:ss")
