0a203e0b6c4893a2009d9a69f0d19b5721058619
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-04-19 17:37:52 +0900

    feat(Connect.py): Clearify the Connect class

--- Connect.py
+++ Connect.py
@@ -1,34 +1,40 @@
 import mysql.connector as con
+
 class Connect():
-    def __init__(self,user,password):
-            self.conn = con.connect(
-                host = "database-1.cdigc6umyoh0.ap-northeast-2.rds.amazonaws.com",
-                port = 3306,
-                user = user,
-                password = password,
-                database ="smartfarmdb"
-                )
-            self.cursor = self.conn.cursor(buffered=True)
+    def __init__(self,User,Password):
+         # DB 연결 시작
+        self.conn = con.connect(
+            host = "database-1.cdigc6umyoh0.ap-northeast-2.rds.amazonaws.com",
+            port = 3306,
+            user = User,
+            password = Password,
+            database ="smartfarmdb"
+            )
+        self.cursor = self.conn.cursor(buffered=True)
 
-    def disConnection(self, conn):
-        conn.close()
+    def disConnection(self):
+        self.conn.close()
 
-    def orderQuery(self, query, addlist = ''):
-        # DB 연결 시작
-        conn, cursor = self.dbConnection()
+    def orderQuery(self, query, addlist, is_ = 'select'):
         # Query 실행
-        cursor.execute(query)
+        self.cursor.execute(query)
 
-        if (len(addlist) == 0):
-            result = cursor.fetchone()
-            # DB 연결 종료
-            self.disConnection(conn)
-            return conn, result
-        else:
-            result = cursor.fetchall()
+        if is_ == 'select':
+            result = self.cursor.fetchall()
             for row in result:
                 addlist.append(row)
-            print(addlist)
+            #print("------------------------")
+            #print(addlist)
             # DB 연결 종료
-            self.disConnection(conn)
-            #return conn, addlist
+            self.disConnection()
+        else:
+            self.conn.commit()  # INSERT, UPDATE, DELETE 등은 commit 필요
+            self.disConnection()
+  
+        
+if __name__ == "__main__" :
+    db_instance = Connect("manager", "0000")
+    addlist = []
+    query = "select * from employees"
+    
+    db_instance.orderQuery(query, addlist)
