c803b75e2b370898ed31cbcf4e2d313313cdf7a6
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-04-23 19:28:32 +0900

    feat(login) : Read the Tag data into 'editId' obj

--- src/gui/login.py
+++ src/gui/login.py
@@ -9,30 +9,10 @@ from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
 from Connect import Connect
+from Receiver import Receiver
 
 login_ui = uic.loadUiType("login.ui")[0]
 register_ui = uic.loadUiType("register.ui")[0]
-
-class Receiver(QThread):
-    detected = pyqtSignal(bytes) #OF
-    c_sensored = pyqtSignal(bytes) #CS
-    tagged =  pyqtSignal(bytes) #ID
-
-    def __init__(self, parent = None):
-        super(Receiver, self).__init__(parent)
-        self.is_running = False
-        print("login recv init")
-
-    def run(self):
-        print("login recv start")
-        self.is_running = True
-        while(self.is_running == True):
-                print("running login recv...")
-                time.sleep(3)
-
-    def stop(self):
-        print("login recv stop")
-        self.is_running = False
 
 class RegisterWindow(QDialog, register_ui):
     def __init__(self, DBconn) :
@@ -54,7 +34,6 @@ class RegisterWindow(QDialog, register_ui):
         id =  self.enterId.text()
         pw = self.enterPw.text()
         goal = self.enterGoal.text()
-        # print(name)
         
         addlist = [] # 각 요소가 길이 6 크기의 튜플(tup)
         fisrt_query = f"select * from employees where ID = \'{id}\'"
@@ -78,8 +57,12 @@ class LoginWindow(QDialog, login_ui):
     def __init__(self, logrecvFlag, DBconn):
         super().__init__()
         self.setupUi(self)
-        #self.uid = "123123"
+        self.uid = None 
         
+        # Tad data 읽어오기 전까지
+        # self.btnLogin.hide()
+        # self.btnRegisternow.hide()
+
         # Placeholder text 설정
         self.editId.setPlaceholderText("ex.83 2B 07 F0")
         self.editPw.setPlaceholderText("ex. nnnn")
@@ -91,14 +74,40 @@ class LoginWindow(QDialog, login_ui):
 
         # DB 인스턴스 파라미터 
         self.DBconn = DBconn
- 
+        
+        # 아두이노와 통신 프로토콜 담당 파라미터
+        self.BTconn = serial.Serial(port='/dev/ttyACM0', baudrate = 9600, timeout = 1)
+
         if logrecvFlag == True:
-            self.recv = Receiver()
+            self.recv = Receiver(self.BTconn)
             self.recv.start()
             print("11111111*login*111111111")
         else:
             print("00000000*login*000000000")
         self.logrecvFlag = logrecvFlag
+    
+        self.recv.tagged.connect(self.tagged)
+
+    def tagged(self, data):
+        print("tagged")
+        if data :
+            addlist = []
+            self.uid = data.decode()
+            print("(((((((((((((((((((())))))))))))))))))))")
+            QMessageBox.information(self, "Tagged Ouput", "Your ID is available now!\n Please input the password!")
+            # Tad data 읽어오면 보이게 하기
+            # self.btnLogin.show()
+            # self.btnRegisternow.show()
+            self.editId.setText(self.uid)
+            print(self.uid)
+            # print(type(self.uid))            
+            query = f"SELECT * FROM employees WHERE ID = \'{self.uid}\'"
+            addlist = self.DBconn.orderQuery(query,addlist)
+            print(addlist)
+            print("(((((((((((((((((((())))))))))))))))))))")
+        else :
+            print(0)
+        return
     
     def PwChanged(self, text):
         # 입력된 텍스트가 비어 있지 않으면 label_4 보이기
@@ -123,14 +132,9 @@ class LoginWindow(QDialog, login_ui):
             # 안전 장치
             if self.logrecvFlag == True:
                 self.recv.stop()
-                #self.logrecvFlag = False
                 # 로그인 성공 시그널 발생
                 self.loginSuccess.emit(id) 
-                # Home 화면에 전달할 uid 정보 업데이트
-                #self.uid = id
-            # else: 
-            #     print("login recv already stopped")
-                
+
         else:
             QMessageBox.warning(self, "Login Output", "Invalid User info!\n Try again or Register new info!")
             self.editId.clear()
